fd39037faa76dde377553afafc2b372163a32458
Chukwuebuka Alaegbu <chukwuebukaalaegbu@gmail.com>
2024-01-17 22:28:31 +0100

    finished the project

--- script.js
+++ script.js
@@ -185,13 +185,37 @@ headCoach.textContent = coachName;
 
 // constructing each of the player card
 const setPlayerCards = (arr = players) => {
-  playerCards.innerHTML += arr.map(({name, position, number, isCaptain, nickname}) => {
-    `
-      <div class="player-card">${name}${isCaptain === true ? "(Captain)" : ""}</div>
+  return playerCards.innerHTML += arr.map(({name, position, number, isCaptain, nickname}) => {
+  return  `
+      <div class="player-card">
+     <h2> ${name}${isCaptain === true ? "(Captain)" : ""}</h2>
       <p>Position: ${position}</p>
       <p>Number: ${number}</p>
       <p>Nickname: ${nickname !== null ? nickname : "N/A"}</p>
+      </div>
     `
-  })
-
+  }).join("")// join removes the comma from array and concats all te item of the array
 }
+playersDropdownList.addEventListener("change", (e) => {
+  playerCards.innerHTML = "";
+  switch (e.target.value){
+      case "nickname":
+        setPlayerCards(players.filter((player) => player.nickname !== null));
+        break;
+      case "forward":
+        setPlayerCards(players.filter((player) => player.position === "forward"));
+        break;
+      case "midfielder":
+        setPlayerCards(players.filter((player) => player.position === "midfielder"));
+        break;
+      case "defender":
+        setPlayerCards(players.filter((player) => player.position === "defender"));
+        break;
+      case "goalkeeper":
+        setPlayerCards(players.filter((player) => player.position === "goalkeeper"));
+        break;
+      default:
+        setPlayerCards()
+        
+  }
+})
