1ab8a98a822209c93aedd9a3aab39e9e915e4fbd
Chukwuebuka Alaegbu <chukwuebukaalaegbu@gmail.com>
2024-01-17 22:41:03 +0100

    final project done

--- script.js
+++ script.js
@@ -188,7 +188,7 @@ const setPlayerCards = (arr = players) => {
   return playerCards.innerHTML += arr.map(({name, position, number, isCaptain, nickname}) => {
   return  `
       <div class="player-card">
-     <h2> ${name}${isCaptain === true ? "(Captain)" : ""}</h2>
+      <h2> ${name}${isCaptain === true ? "(Captain)" : ""}</h2>
       <p>Position: ${position}</p>
       <p>Number: ${number}</p>
       <p>Nickname: ${nickname !== null ? nickname : "N/A"}</p>
